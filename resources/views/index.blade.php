d5dba5eb598a302ca4a9bf65cadb90afbf4e665a
TechnoDexx <itshark@mail.ru>
2022-03-10 16:41:29 +0300

    Setup a fresh Laravel App

---
+++ resources/views/index.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Объявления</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="my-3 text-center">Объявления</h1>
+        @if(count($bbs) > 0)
+        <table class="table table-stripped">
+            <thead>
+                <tr>
+                    <th>Товар</th>
+                    <th>Цена, руб.</th>
+                    <th>&nbsp;</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($bbs as $bb)
+                <tr>
+                    <td>
+                        <h3>{{ $bb->title }}</h3>
+                    </td>
+                    <td>{{ $bb->price }}</td>
+                    <td>
+                        <a href="/{{ $bb->id }}">Подробнее...</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        @endif
+    </div>
+</body>
+
+</html>
