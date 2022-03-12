df11b7146d19e0a7adff2fe9ac32ae0270d99653
TechnoDexx <itshark@mail.ru>
2022-03-12 12:53:18 +0300

    Setup a fresh Laravel App

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.base')
+@section('title','Вход')
 
-@section('content')
+@section('main')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
