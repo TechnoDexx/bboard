6fb31abef93a170720149dc2df198587129a0a30
TechnoDexx <itshark@mail.ru>
2022-03-18 14:21:32 +0300

    Setup a fresh Laravel  App

--- database/factories/BbFactory.php
+++ database/factories/BbFactory.php
@@ -3,12 +3,15 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Bb;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Bb>
  */
 class BbFactory extends Factory
 {
+    protected $model = Bb::class;
     /**
      * Define the model's default state.
      *
@@ -16,8 +19,6 @@ class BbFactory extends Factory
      */
     public function definition()
     {
-        return [
-            //
-        ];
+        return [];
     }
 }
